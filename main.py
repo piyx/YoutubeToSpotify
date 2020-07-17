592adc2ec961038ca610b729389821e3775af60e
V Abhishek Kumar <54227835+piyx@users.noreply.github.com>
2020-07-17 22:12:34 +0530

    added all files

---
+++ main.py
@@ -0,0 +1,31 @@
+from yt import Youtube
+from sptfy import Spotify
+
+
+def main():
+    # Playlist url
+    playlist_url = input("Enter youtube playlist url: ")
+
+    # Youtube instance
+    yt = Youtube(playlist_url)
+    song_titles = yt.get_songs_title()
+    print(len(song_titles))
+    songs_info = yt.get_songs_info(song_titles)
+    print(len(songs_info))
+
+    # Spotify instance
+    sptfy = Spotify()
+
+    playlst_id = sptfy.create_playlist("Youtube songs")
+
+    for song_name, artist in songs_info.items():
+        uri = sptfy.get_spotify_uri(artist, song_name)
+        status = sptfy.add_songs_to_playlist(playlst_id, {"uris": [uri]})
+        if status:
+            print(f"{artist}-{song_name} was added to playlist.")
+        else:
+            print(f"\nERROR!! {artist}-{song_name} could not be added.\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ secrts.py
@@ -0,0 +1,2 @@
+SPTFY_TOKEN = "your-spotiy-token"  # The token needs to refreshed once per hour
+USER_ID = "your-spotiy-user-id"
