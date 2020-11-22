a44a2e108adcc86ba81be536802847fabb8cc9d3
piyx <piyx.abhishek@gmail.com>
2020-11-22 20:38:56 +0530

    complete rewrite
    Former-commit-id: 5105bedd9664fb8fa40b4fe019b68e412d6f9469 [formerly 0925c971d968f620ed45a8de92e87af6e294c022] [formerly 52b1fb945cbaec55e361ef963abfd3ebfd79b322 [formerly 0fca38233b3be02f4650abe97d0a0017cf31735f]]
    Former-commit-id: 1cb434c4b9103f45f661b69bd6b7dc1f7c3345b0 [formerly 033831daaec7fb8b3c403bdfd1888a3335bcc351]
    Former-commit-id: 34b40208b83f12c6d6843ed3f3ee12d4d53f13c0
    Former-commit-id: f87a43066c90f05c3b914338dbf1a91e86eba634

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
-from yt import Youtube
-from sptfy import Spotify
-
+from spotify import Spotify
+from youtube import Youtube
 
 def main():
-    # Playlist url
-    playlist_url = input("Enter youtube playlist url: ")
-    playlist_name = input("Enter playlist name: ")
+    sp = Spotify()
+    yt = Youtube()
+    
+    yt_playlist_id = input("Enter youtube playlist id: ")
+    spotify_playlist_name = input("Enter a name for your spotify playlist: ")
+    songs = yt.get_songs_from_playlist(yt_playlist_id)
+    spotify_playlist_id = sp.create_playlist(spotify_playlist_name)
 
-    # Youtube instance
-    yt = Youtube(playlist_url)
-    song_titles = yt.get_songs_title()
-    print(len(song_titles))
-    songs_info = yt.get_songs_info(song_titles)
-    print(len(songs_info))
-
-    # Spotify instance
-    sptfy = Spotify()
-    playlst_id = sptfy.create_playlist(playlist_name)
-
-    for song_name, artist in songs_info.items():
-        uri = sptfy.get_spotify_uri(artist, song_name)
-        status = sptfy.add_songs_to_playlist(playlst_id, {"uris": [uri]})
-        if status:
-            print(f"{artist}-{song_name} was added to playlist.")
-        else:
-            print(f"\nERROR!! {artist}-{song_name} could not be added.\n")
+    for song in songs:
+        song_uri = sp.get_song_uri(song.artist, song.title)
+        was_added = sp.add_song_to_playlist(song_uri, spotify_playlist_id)
+        if was_added:
+            print(f'{song.artist} - {song.title} was added to playlist.')
 
 
 if __name__ == "__main__":
