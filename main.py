b440b07fef8410720720aaf5432a9a36317025c7
AltNyx <razorabhishek007@gmail.com>
2020-12-07 13:49:26 +0530

    ensure token loads before reading youtube songs

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ def main():
     
     yt_playlist_id = input("Enter youtube playlist id: ")
     spotify_playlist_name = input("Enter a name for your spotify playlist: ")
-    songs = yt.get_songs_from_playlist(yt_playlist_id)
     spotify_playlist_id = sp.create_playlist(spotify_playlist_name)
+    songs = yt.get_songs_from_playlist(yt_playlist_id)
 
     for song in songs:
         song_uri = sp.get_song_uri(song.artist, song.title)
+
+        if not song_uri:
+            print(f"{song.artist} - {song.title} was not found!")
+            continue
+        
         was_added = sp.add_song_to_playlist(song_uri, spotify_playlist_id)
+
         if was_added:
             print(f'{song.artist} - {song.title} was added to playlist.')
 
